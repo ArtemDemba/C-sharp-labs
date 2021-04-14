7b2517b568f1027cae901d3801354c8495972ac8
ArtemDemba <DVL.45@mail.ru>
2021-04-14 19:50:29 +0300

    lab 3

---
+++ lab 2/English letters/English letters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+namespace English_letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp;
+            Console.WriteLine("Input string: ");
+            temp = Console.ReadLine();
+            StringBuilder str = new StringBuilder(temp);
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (str[j] < 97 || str[j] > 122)        //checking that all letters are english
+                {
+                    Console.WriteLine("Not all letters from the Endlish alphabet!");
+                    return;
+                }
+            }
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'
+                    || str[i] == 'y')
+                {
+                    if (str[i + 1] == 'z')
+                    {
+                        str[i + 1] = 'a';       //'z' change to 'a'
+                    }
+                    else
+                    {
+                        str[i + 1]++;           //replacing a letter with the next alphabetically
+                    }
+                }
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
+
---
+++ lab 2/Generating/Generating/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace Generating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random RandLeft = new Random();
+            Random RandRight = new Random();
+            string str;
+            str = Console.ReadLine();
+            char[] resultArray = new char[str.Length];      //original array
+            char temp;
+            int randFirst, randSecond;                      
+            for (int i = 0; i < str.Length; i++)            //entering data from the string type into
+            {                                               //char array
+                resultArray[i] = str[i];
+            }
+            for (int i = 0; i < str.Length * 5; i++)        //determination of the number of permutations
+            {
+                randFirst = RandLeft.Next(str.Length);
+                randSecond = RandRight.Next(str.Length);
+                while (randFirst == randSecond)             //checking, to randFirst and randSecond
+                {                                           //arent coincide
+                    randSecond = RandRight.Next(str.Length);
+                }
+                temp = resultArray[randFirst];                //swap
+                resultArray[randFirst] = resultArray[randSecond];
+                resultArray[randSecond] = temp;
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.Write(resultArray[i]);
+            }
+        }
+    }
+}
